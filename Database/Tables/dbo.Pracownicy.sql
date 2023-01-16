7c595b62d871b802f4ac3dacbadbfde3337e0cd5
D4gull <67342410+D4gull@users.noreply.github.com>
2023-01-16 05:42:03 +0100

    Database

---
+++ Database/Tables/dbo.Pracownicy.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Pracownicy] (
+  [Id_Pracownicy] [int] IDENTITY (1000, 1),
+  [Imię] [varchar](30) NOT NULL,
+  [Nazwisko] [varchar](50) NOT NULL,
+  [Id_Rola] [int] NULL,
+  [Data_Utworzenia] [date] NOT NULL,
+  PRIMARY KEY CLUSTERED ([Id_Pracownicy])
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Pracownicy]
+  ADD FOREIGN KEY ([Id_Rola]) REFERENCES [dbo].[Rola] ([Id_Rola]) ON UPDATE CASCADE
+GO
