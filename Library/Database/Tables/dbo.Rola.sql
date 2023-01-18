65b1b98c11789b958226204aa05f2a3f138c86e1
D4gull <67342410+D4gull@users.noreply.github.com>
2023-01-18 20:33:19 +0100

    reprain

--- Library/Database/Tables/dbo.Rola.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Rola] (
-  [Id_Rola] [int] IDENTITY (100, 15),
-  [Nazwa] [varchar](50) NOT NULL,
-  PRIMARY KEY CLUSTERED ([Id_Rola])
-)
-ON [PRIMARY]
-GO
--- Library/Database/Tables/dbo.Wydawnictwo.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Wydawnictwo] (
-  [Id_Wydawnictwa] [int] IDENTITY (100, 15),
-  [Nazwa] [varchar](50) NOT NULL,
-  PRIMARY KEY CLUSTERED ([Id_Wydawnictwa])
-)
-ON [PRIMARY]
-GO
